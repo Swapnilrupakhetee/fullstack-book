dc81c388d45a0557591fa09f6fa3601a89bf2b96
Swapnilrupakhetee <rupakheteeswapnil@gmail.com>
2024-11-19 22:01:23 +0545

    sending-http-req

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express= require('express');
-require('dotenv').config //need to import dotenv
-const PORT=process.env.PORT || 7001
+const dotenv=require('dotenv').config(); //need to import dotenv
+const PORT=process.env.PORT 
 const app=express();
+
 
 app.get('/',(req,res)=>{
     res.send('Welcome to the API');
 })
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`)
